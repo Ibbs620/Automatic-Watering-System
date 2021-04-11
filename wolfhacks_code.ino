169c6f3cc951ce147e08375e3338e71a6ef2f78a
Ibrahim Quraishi <58309356+Ibbs620@users.noreply.github.com>
2021-04-11 16:06:18 -0400

    updated code with comments and bug fixes

--- wolfhacks_code.ino
+++ wolfhacks_code.ino
@@ -3,32 +3,29 @@
 #include <ds3231.h>
 #include "DHT.h"
 #include <LiquidCrystal_I2C.h>
+//Import Libraries
 
-#define DHTTYPE DHT11
+#define DHTTYPE DHT11 //Define type of temperature sensor being used
 
 Servo water;
 DHT dht(2, DHTTYPE); 
 LiquidCrystal_I2C lcd(0x27,16,2);
+//Declare LCD, servo, DHT sensor
 
 struct ts t; 
-float sensorValue = 0; 
-int watered = 0;
 
 void setup() {
-  water.attach(3);
-  DS3231_init(DS3231_CONTROL_INTCN);  
-  waterPlants();
-  //applyAluminumSulfate();
-  Serial.begin(9600);
+  water.attach(3); //Set servo as pin 3
+  DS3231_init(DS3231_CONTROL_INTCN); //Start up temperature sensor
   Wire.begin();
   dht.begin();
   lcd.init();
   lcd.backlight();
+  //start up components
 }
 
 void loop() {
-  DS3231_get(&t);
-
+  DS3231_get(&t); //get time variable from clock
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
@@ -46,8 +43,7 @@ void loop() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
-  
-  int val = map(analogRead(A0),550,0,0,100) + 100;
+  int val = map(analogRead(A0),550,0,0,100) + 100; //Read soil moisture sensor and fix readings
 
   lcd.setCursor(0,0);
   lcd.print(printDateTime());
@@ -59,36 +55,23 @@ void loop() {
   lcd.print("Mstr:");
   lcd.setCursor(14,1);
   lcd.print(val);
-  delay(1000);
+  //print information to LCD
 
-  if(val < 18 && watered % 5 == 0){
-    applyAluminumSulfate();
-    watered++;
-  } else if(val < 18){
+ if(val < 18){ //water plants when soil is dry
     waterPlants();
-    watered++;
-  }
+ }
+ delay(1000); //delay
 }
-
-
-void waterPlants(){
-  for(int i = 90; i < 180; i++){
+void waterPlants(){ //Waters the plants with the servo
+  for(int i = 90; i < 180; i++){ //slowly push servo. If pushed fast, the water will shoot out too fast
     water.write(i);
     delay(50);
   }
-  water.write(180);
+  water.write(180); //return servo to original position
   delay(1000);
   water.write(90);
 }
 
-void applyAluminumSulfate(){
-  water.write(90);
-  delay(1000);
-  water.write(0);
-  delay(1000);
-  water.write(90);
-}
-
-String printDateTime(){
+String printDateTime(){ //format date and time for LCD display
   return String(t.mon) + "/" + String(t.mday) + "/" + String(t.year) + " " + String(t.hour) + ":" + String(t.min);
 }
